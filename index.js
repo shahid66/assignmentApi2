b62302c5d086ceb917b38f8abd05684f9f7fe41e
shahid66 <kk.shahid66@gmail.com>
2022-10-21 23:29:38 +0600

    make it better5

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ app.use(express.static('public'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors(
+    {
+        origin:"http://localhost:300"
+    }
+));
 
 // DB Connection
 mongoose
